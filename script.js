f08bc8eb73b9f75f1da91dec87e759f2f37d289c
stacy murenzi <stacymurenzi@gmail.com>
2019-07-19 20:59:37 +0200

    added if statement

--- script.js
+++ script.js
@@ -5,32 +5,49 @@ function weekd() {
     var c = (y - 1) / 100 + 1;
     var dayoftheweek = (c / 4 - 2 * c - 1 + 5 * y / 4 + 26 * (m + 1) / 10 + d) % 7;
     console.log(Math.round(dayoftheweek));
-
-
-
-
-
-
-
     var gabo = ["kwasi", "kwadwo", "kwabena", "kwaku", "yaw", "kofi", "kwame"];
     var gore = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     var choice = document.getElementById('select').value;
+    if (d >= 31 || d <= 1) {
 
-    if (choice === "male") {
-        console.log(gabo[Math.round(dayoftheweek)]);
-
-        document.getElementById('akan').innerHTML = gabo[Math.round(dayoftheweek)];
-
-
-    } else if (choice === "female") {
-
-
-        console.log(gore[Math.round(dayoftheweek)]);
-        document.getElementById('akan').innerHTML = gore[Math.round(dayoftheweek)];
+        alert("ivalid input enter days less than 31 or greater than 0");
+    } else if (m > 12 || m <= 1) {
+        alert("invalid input enter month less than 12 or greater than 0");
     } else {
-        console.log("invalid output");
+
+
+
+
+
+
+        if (choice === "male") {
+            console.log(gabo[Math.round(dayoftheweek)]);
+
+            document.getElementById('akan').innerHTML = gabo[Math.round(dayoftheweek)];
+
+
+        } else if (choice === "female") {
+
+
+            console.log(gore[Math.round(dayoftheweek)]);
+            document.getElementById('akan').innerHTML = gore[Math.round(dayoftheweek)];
+
+        } else {
+            alert("no information provided");
+
+        }
     }
 
-
-
 }
+
+// function validity() {
+//     var d;
+//     var m;
+//     if (d > 31 || d < 0) {
+
+//         alert("ivalid input enter days less than 31 or greater than 0");
+//     }
+//     if (m > 12 || m < 1) {
+//         alert("invalid input enter month less than 12 or greater than 0");
+//     }
+// }
